7d6660e1346d93614235023ae28de8c5dfbd0ab1
Marianthi Karadeloglou <up1072757@upnet.gr>
2023-03-02 22:52:53 +0200

    Did the 4th part of the lab1 exercise

--- lab1-1.py
+++ lab1-1.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 #Ask for URL from user
 url = input ("Enter URL here: ")
@@ -10,13 +11,14 @@ response = requests.get(url)
 print("Headers of the http response:\n")
 for name,value in response.headers.items():
     print("{:20s} {}".format(name,value))
-    
-#print(response.headers)
 
 #Find server software 
 software = response.headers.get('Server')
 #Print server software
-print(f"\nThe server software is: {software}")
+print(f"\nThe server software is: {software}\n")
 
 #Check for cookies and print
-#cookies = response.cookies 
+print("Cookies:")
+cookies = response.cookies 
+for cookie, value in enumerate(cookies):
+    print("Cookie Name: {}   Expires in {} days".format(value.name,round((value.expires-time.time())/86400)))
