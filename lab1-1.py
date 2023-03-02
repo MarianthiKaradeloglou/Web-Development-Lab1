038b0a74f16853b715d1ae72f6e1d81c0c3de4bc
Marianthi Karadeloglou <up1072757@upnet.gr>
2023-03-02 21:46:46 +0200

    Added comments to LAB1.1,2,3

--- lab1-1.py
+++ lab1-1.py
@@ -1,8 +1,12 @@
 import requests
 
+#Ask for URL from user
 url = input ("Enter URL here: ")
 
+#Make the http request for the url
 response = requests.get(url)
 
+#Print headers
 print("Headers of the http response:\n")
 print(response.headers)
+
